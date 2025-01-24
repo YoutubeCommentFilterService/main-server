a38679ae67bc6ffe31a2150eec58727d5a5ec408
PromotezCitizen <gkstkdgus821@gmail.com>
2025-01-24 14:54:45 +0900

    security cors 패턴 추가

--- src/main/java/com/hanhome/youtube_comments/oauth/config/SecurityConfig.java
+++ src/main/java/com/hanhome/youtube_comments/oauth/config/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:5173", "https://accounts.google.com"));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:5173", "https://accounts.google.com", "http://127.0.0.1:5173"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
