0a59ece69f5267170b60bfccd7977927bc4d665f
PromotezCitizen <gkstkdgus821@gmail.com>
2025-01-24 14:53:28 +0900

    redis에서 pattern 검색 후 삭제하는 로직 작성

--- src/main/java/com/hanhome/youtube_comments/redis/service/RedisService.java
+++ src/main/java/com/hanhome/youtube_comments/redis/service/RedisService.java
@@ -1,9 +1,12 @@
 package com.hanhome.youtube_comments.redis.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.Cursor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -29,5 +32,16 @@ public class RedisService {
 
     public void remove(String key) {
         redisTemplate.delete(key);
+    }
+
+    public void searchNRemove(String pattern, boolean isPrefix) {
+        pattern = isPrefix ? pattern+"*" : "*" + pattern;
+        ScanOptions scanOptions = ScanOptions.scanOptions().match(pattern).count(100).build();
+        Cursor<byte[]> cursor =  redisTemplate.getConnectionFactory().getConnection().scan(scanOptions);
+
+        while(cursor.hasNext()) {
+            String key = new String(cursor.next(), StandardCharsets.UTF_8);
+            remove(key);
+        }
     }
 }
