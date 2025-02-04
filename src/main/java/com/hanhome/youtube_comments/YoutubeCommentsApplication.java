22fe3be9cec74f0a22cc19b31d488ef8ca2f1b34
PromotezCitizen <gkstkdgus821@gmail.com>
2025-02-04 15:50:17 +0900

    property scan 추가

--- src/main/java/com/hanhome/youtube_comments/YoutubeCommentsApplication.java
+++ src/main/java/com/hanhome/youtube_comments/YoutubeCommentsApplication.java
@@ -2,8 +2,10 @@ package com.hanhome.youtube_comments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class YoutubeCommentsApplication {
 
 	public static void main(String[] args) {
