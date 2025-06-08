323c2e14ea4bd912a7587873dd6891ca0003e91f
PromotezCitizen <gkstkdgus821@gmail.com>
2025-06-09 00:49:51 +0900

    필요 없어진 DTO 필드 주석처리

--- src/main/java/com/hanhome/youtube_comments/member/object/YoutubeAccountDetail.java
+++ src/main/java/com/hanhome/youtube_comments/member/object/YoutubeAccountDetail.java
@@ -2,13 +2,15 @@ package com.hanhome.youtube_comments.member.object;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Builder
 @Getter
+@ToString
 public class YoutubeAccountDetail {
     private String channelId;
     private String playlistId;
-    private String channelName;
+//    private String channelName;
     private String channelHandler;
-    private String thumbnailUrl;
+//    private String thumbnailUrl;
 }
