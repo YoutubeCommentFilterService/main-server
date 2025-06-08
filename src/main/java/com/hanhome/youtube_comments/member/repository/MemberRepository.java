5e411f24f06e50724f4dec89af8a503b157e2a52
PromotezCitizen <gkstkdgus821@gmail.com>
2025-06-09 00:45:11 +0900

    Member Repository JPA 함수 추가

--- src/main/java/com/hanhome/youtube_comments/member/repository/MemberRepository.java
+++ src/main/java/com/hanhome/youtube_comments/member/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package com.hanhome.youtube_comments.member.repository;
 
 import com.hanhome.youtube_comments.member.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +10,7 @@ import java.util.UUID;
 
 public interface MemberRepository extends JpaRepository<Member, UUID> {
     Optional<Member> findByEmail(String email);
+    Optional<Member> findByChannelId(String channelId);
+    Page<Member> findByIdNot(UUID id, Pageable pageable);
+    void deleteByEmail(String email);
 }
