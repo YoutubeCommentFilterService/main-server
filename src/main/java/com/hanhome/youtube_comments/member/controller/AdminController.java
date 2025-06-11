47cc4dcfac23e1fec1a52789ad20de998ff6993d
PromotezCitizen <gkstkdgus821@gmail.com>
2025-06-11 17:00:27 +0900

    권한 업데이트 로직 추가
    - 권한 업데이트
       - 가능한 권한들 가져오는 로직 및 권한 업데이트 로직 작성
       - 가능한 권한들은 캐시를 통해 응답 속도 빠르게 함
    - 검색 패턴에 따라 전체 유저를 검색할 것인지, 특정 패턴을 가진 유저를 검색할 것인지 선택

--- src/main/java/com/hanhome/youtube_comments/member/controller/AdminController.java
+++ src/main/java/com/hanhome/youtube_comments/member/controller/AdminController.java
@@ -1,7 +1,9 @@
 package com.hanhome.youtube_comments.member.controller;
 
+import com.hanhome.youtube_comments.member.dto.AvailableRolesDto;
 import com.hanhome.youtube_comments.member.dto.DeleteUserDto;
-import com.hanhome.youtube_comments.member.dto.GetUserDto;
+import com.hanhome.youtube_comments.member.dto.GetMemberDto;
+import com.hanhome.youtube_comments.member.dto.UpdateUserRoleDto;
 import com.hanhome.youtube_comments.member.service.AdminService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +17,24 @@ public class AdminController {
     private final AdminService adminService;
 
     @GetMapping("/users")
-    public ResponseEntity<GetUserDto.Response> getUsers(@Validated @ModelAttribute GetUserDto.Request pagination) {
+    public ResponseEntity<GetMemberDto.Response> getUsers(@Validated @ModelAttribute GetMemberDto.Request pagination) {
         return ResponseEntity.ok(adminService.getUsers(pagination));
     }
 
-    @DeleteMapping
-    public ResponseEntity<?> deleteUSer(@ModelAttribute DeleteUserDto.Request request) throws Exception {
-        adminService.deleteUser(request);
-        return ResponseEntity.accepted().build();
+    @GetMapping("/roles")
+    public ResponseEntity<AvailableRolesDto.Response> getAvailableRoles() {
+        return ResponseEntity.ok(adminService.getAvailableRoles());
+    }
+
+    @PatchMapping("/member/role")
+    public ResponseEntity<?> updateUserRole(@RequestBody UpdateUserRoleDto.Request updateDto) {
+        boolean result = adminService.updateUserRole(updateDto);
+        return result ? ResponseEntity.ok().build() : ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/member")
+    public ResponseEntity<?> deleteUSer(@ModelAttribute DeleteUserDto.Request requestDto) throws Exception {
+        adminService.deleteUser(requestDto);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/hanhome/youtube_comments/member/service/AdminService.java
+++ src/main/java/com/hanhome/youtube_comments/member/service/AdminService.java
@@ -1,12 +1,13 @@
 package com.hanhome.youtube_comments.member.service;
 
-import com.hanhome.youtube_comments.member.dto.DeleteUserDto;
-import com.hanhome.youtube_comments.member.dto.GetUserDto;
+import com.hanhome.youtube_comments.member.dto.*;
 import com.hanhome.youtube_comments.member.entity.Member;
+import com.hanhome.youtube_comments.member.object.MemberRole;
 import com.hanhome.youtube_comments.member.object.SimpleMemberInfo;
 import com.hanhome.youtube_comments.member.repository.MemberRepository;
 import com.hanhome.youtube_comments.oauth.dto.CustomUserDetails;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -15,6 +16,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -26,19 +28,24 @@ public class AdminService {
     private final MemberService memberService;
 
     @PreAuthorize("hasRole('ADMIN')")
-    public GetUserDto.Response getUsers(GetUserDto.Request pagination) {
+    public GetMemberDto.Response getUsers(GetMemberDto.Request pagination) {
         int page = pagination.getPage() - 1;
         int take = pagination.getTake();
+        String searchPattern = pagination.getChannelName();
 
         CustomUserDetails userDetails = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         Sort.Direction direction = Sort.Direction.fromString("DESC");
         PageRequest pageable = PageRequest.of(page, take, Sort.by(direction, "createdAt"));
 
-        Page<Member> result = memberRepository.findByIdNot(userDetails.getMember().getId(), pageable);
+        UUID adminUUID = userDetails.getMember().getId();
+
+        Page<Member> result = (searchPattern == null || searchPattern.trim().isEmpty())
+            ? memberRepository.findByIdNot(adminUUID, pageable)
+            : memberRepository.findByChannelNameContainingAndIdNot(searchPattern.trim(), adminUUID, pageable);
         List<SimpleMemberInfo> members = result.get().map(Member::toSimpleInfo).toList();
 
-        return GetUserDto.Response.builder()
+        return GetMemberDto.Response.builder()
                 .totalMembers(result.getTotalElements())
                 .totalPages(result.getTotalPages())
                 .members(members)
@@ -47,7 +54,7 @@ public class AdminService {
 
     @PreAuthorize("hasRole('ADMIN')")
     @Transactional
-    public void deleteUser(DeleteUserDto.Request request) throws Exception {
+    public void deleteUser(DeleteUserDto.Request request) {
         String targetChannelId = request.getChannelId();
         UUID targetUserId = UUID.fromString(request.getUserId());
         Optional<Member> targetMember = memberRepository.findByChannelId(targetChannelId)
@@ -56,5 +63,32 @@ public class AdminService {
             UUID targetUserUUID = targetMember.get().getId();
             memberService.withdraw(targetUserUUID);
         }
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @Transactional
+    public boolean updateUserRole(UpdateUserRoleDto.Request updateDto) {
+        System.out.println(updateDto);
+        UUID targetUserId = UUID.fromString(updateDto.getUserId());
+        String role = updateDto.getRole();
+
+        MemberRole toUpdateRole = MemberRole.valueOf(role);
+
+        Optional<Member> memberOpt = memberRepository.findById(targetUserId);
+        memberOpt.ifPresent(member -> member.setRole(toUpdateRole));
+
+        return memberOpt.isPresent();
+    }
+
+    @Cacheable(value = "availableRoles")
+    public AvailableRolesDto.Response getAvailableRoles() {
+        List<String> availableRoles = Arrays.stream(MemberRole.values())
+                .filter(role -> role != MemberRole.UNLINKED)
+                .map(Enum::name)
+                .toList();
+        System.out.println(availableRoles);
+        return AvailableRolesDto.Response.builder()
+                .availableRoles(availableRoles)
+                .build();
     }
 }
