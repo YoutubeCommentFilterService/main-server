bb209593d0c45a0859c94b858bcf177a00334d7a
PromotezCitizen <gkstkdgus821@gmail.com>
2025-06-13 19:21:54 +0900

    필터가 자동 적용되는 현상 수정
    - @Component 어노테이션이 붙어있으면 무조건 적용해버린다.

--- src/main/java/com/hanhome/youtube_comments/oauth/filter/CsrfDebugFilter.java
+++ src/main/java/com/hanhome/youtube_comments/oauth/filter/CsrfDebugFilter.java
@@ -13,7 +13,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.Arrays;
 
-@Component
+//@Component
 public class CsrfDebugFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
