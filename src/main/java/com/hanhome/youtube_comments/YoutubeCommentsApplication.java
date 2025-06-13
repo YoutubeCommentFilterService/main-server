d9cb4b891e2caad30642697ae6ed9aeddaff8191
PromotezCitizen <gkstkdgus821@gmail.com>
2025-06-13 19:22:48 +0900

    batch 적용에 필요한 사전 설정 추가

--- src/main/java/com/hanhome/youtube_comments/YoutubeCommentsApplication.java
+++ src/main/java/com/hanhome/youtube_comments/YoutubeCommentsApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableMongoRepositories
 @EnableCaching
+@EnableScheduling
 public class YoutubeCommentsApplication {
 
 	public static void main(String[] args) {
