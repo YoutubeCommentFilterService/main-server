053ec8cd60145a364b5ab60487fc6a882df087f6
PromotezCitizen <gkstkdgus821@gmail.com>
2025-06-15 17:22:09 +0900

    매주 월요일 새벽 1시에 유저 프로필 업데이트 작업 진행

---
+++ src/main/java/com/hanhome/youtube_comments/member/batch/YoutubeChannelInfoUpdater.java
@@ -0,0 +1,43 @@
+package com.hanhome.youtube_comments.member.batch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class YoutubeChannelInfoUpdater {
+    private final JobLauncher jobLauncher;
+    private final Job updateMemberProfileJob;
+
+    public YoutubeChannelInfoUpdater(
+            JobLauncher jobLauncher,
+            @Qualifier("updateMemberProfileJob") Job updateMemberProfileJob
+    ) {
+        this.jobLauncher = jobLauncher;
+        this.updateMemberProfileJob = updateMemberProfileJob;
+    }
+
+    @Scheduled(cron = "0 0 1 * * 1", zone = "Asia/Seoul")
+    public void run() {
+        try {
+            String dynamicJobName = "updateChannelInfo-" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("mm-ss-SSS"));
+
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .toJobParameters();
+
+            jobLauncher.run(
+                    updateMemberProfileJob,
+                    jobParameters
+            );
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
