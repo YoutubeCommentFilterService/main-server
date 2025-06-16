6b3a2b283a5f52c5e4c4f4cf60be0e81a1e8cdd8
PromotezCitizen <gkstkdgus821@gmail.com>
2025-06-16 23:12:44 +0900

    job test code 추가

---
+++ src/main/java/com/hanhome/youtube_comments/member/batch/TestLauncher.java
@@ -0,0 +1,50 @@
+package com.hanhome.youtube_comments.member.batch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+// 아래의 어노테이션이 없으면 실행되지 않는다.
+//@Component
+public class TestLauncher implements ApplicationRunner {
+    private final JobLauncher jobLauncher;
+    private final Job job;
+
+    public TestLauncher(
+        JobLauncher jobLauncher,
+        @Qualifier("") Job job
+    ) {
+        this.jobLauncher = jobLauncher;
+        this.job = job;
+    }
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("Test Runner Started!");
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("time", System.currentTimeMillis())
+                .toJobParameters();
+
+        try {
+            // JobLauncher를 사용하여 Job 실행
+            jobLauncher.run(job, jobParameters);
+            System.out.println("Spring Batch Job 'testing' executed successfully!");
+        } catch (JobExecutionAlreadyRunningException e) {
+            System.err.println("Job 'testing' is already running: " + e.getMessage());
+        } catch (JobRestartException e) {
+            System.err.println("Job 'testing' cannot be restarted: " + e.getMessage());
+        } catch (JobInstanceAlreadyCompleteException e) {
+            System.err.println("Job 'testing' has already completed for these parameters: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("Error executing Job 'testing': " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
