2eb0360adfcde5bdc6a4d33d8c8a270f301704c2
PromotezCitizen <gkstkdgus821@gmail.com>
2025-06-18 21:09:10 +0900

    controlelr -> restcontroller

--- src/main/java/com/hanhome/youtube_comments/google/controller/PredictClassController.java
+++ src/main/java/com/hanhome/youtube_comments/google/controller/PredictClassController.java
@@ -4,11 +4,11 @@ import com.hanhome.youtube_comments.google.dto.PredictCategoryDto;
 import com.hanhome.youtube_comments.google.service.PredictClassService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/metadata/predict-class")
 public class PredictClassController {
