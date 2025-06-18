a874f2537b92f33ffe3420f84270f594c9e5fe66
PromotezCitizen <gkstkdgus821@gmail.com>
2025-06-18 21:08:15 +0900

    record 매개변수 순서 변경

--- src/main/java/com/hanhome/youtube_comments/oauth/dto/CustomTokenRecord.java
+++ src/main/java/com/hanhome/youtube_comments/oauth/dto/CustomTokenRecord.java
@@ -2,4 +2,4 @@ package com.hanhome.youtube_comments.oauth.dto;
 
 import java.util.concurrent.TimeUnit;
 
-public record CustomTokenRecord(long ttl, String token, TimeUnit timeUnit) {}
+public record CustomTokenRecord(String token, TimeUnit timeUnit, long ttl) {}
