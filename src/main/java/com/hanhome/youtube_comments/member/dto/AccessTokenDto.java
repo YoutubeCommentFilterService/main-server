908de7f1bf14215f2ae8b118fb65be649eb889ce
PromotezCitizen <gkstkdgus821@gmail.com>
2025-06-18 21:10:19 +0900

    사용하지 않는 DTO 제거 및 Role 반환 시 ENUM 사용

--- src/main/java/com/hanhome/youtube_comments/member/dto/AccessTokenDto.java
+++ src/main/java/com/hanhome/youtube_comments/member/dto/AccessTokenDto.java
@@ -1,5 +1,6 @@
 package com.hanhome.youtube_comments.member.dto;
 
+import com.hanhome.youtube_comments.member.object.MemberRole;
 import com.hanhome.youtube_comments.oauth.dto.CustomTokenRecord;
 import lombok.Builder;
 import lombok.Getter;
@@ -7,20 +8,11 @@ import lombok.Getter;
 public class AccessTokenDto {
     @Builder
     @Getter
-    public static class Response {
-        private String profileImage;
-        private String nickname;
-        private Boolean hasYoutubeAccess;
-        private String role;
-    }
-
-    @Builder
-    @Getter
     public static class Renew {
         private CustomTokenRecord customTokenRecord;
         private String profileImage;
         private String nickname;
         private Boolean hasYoutubeAccess;
-        private String role;
+        private MemberRole role;
     }
 }
