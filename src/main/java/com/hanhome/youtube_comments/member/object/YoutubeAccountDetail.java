099e7da3b222bc2b5be00174d407b1b059236920
PromotezCitizen <gkstkdgus821@gmail.com>
2025-06-20 16:51:58 +0900

    AccountDetail Detail 값 수정

--- src/main/java/com/hanhome/youtube_comments/member/object/YoutubeAccountDetail.java
+++ src/main/java/com/hanhome/youtube_comments/member/object/YoutubeAccountDetail.java
@@ -1,5 +1,6 @@
 package com.hanhome.youtube_comments.member.object;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,7 +9,20 @@ import lombok.Getter;
 public class YoutubeAccountDetail {
     private String channelId;
     private String playlistId;
-//    private String channelName;
+    private String channelName;
     private String channelHandler;
-//    private String thumbnailUrl;
+    private String thumbnailUrl;
+    private Integer subscriberCount;
+
+    @AllArgsConstructor
+    @Getter
+    public static class HandlerUrlMapper {
+        private String thumbnailUrl;
+        private String channelHandler;
+        private Integer subscriberCount;
+    }
+
+    public HandlerUrlMapper mapToHandlerUrl() {
+        return new HandlerUrlMapper(thumbnailUrl, channelHandler, subscriberCount);
+    }
 }
