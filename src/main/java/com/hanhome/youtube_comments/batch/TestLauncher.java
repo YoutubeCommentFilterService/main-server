295bf10c5e24f2579da7aad5fd31e74cd49bcef4
PromotezCitizen <gkstkdgus821@gmail.com>
2025-06-21 00:22:35 +0900

    TestLauncher 업데이트

--- src/main/java/com/hanhome/youtube_comments/batch/TestLauncher.java
+++ src/main/java/com/hanhome/youtube_comments/batch/TestLauncher.java
@@ -10,6 +10,7 @@ import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
 // 아래의 어노테이션이 없으면 실행되지 않는다.
 //@Component
